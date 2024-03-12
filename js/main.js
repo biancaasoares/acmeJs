da8a3eef1af3ff3fd8af0f4c12704a185994fa3a
Bianca Soares <biannca01soares@gmail.com>
2024-03-12 08:55:08 -0300

    start

---
+++ js/main.js
@@ -0,0 +1,25 @@
+import { getFilmes, getFilme } from "./filmes.js";
+
+function preencherCards(filemes){
+    const container = document.createElement('div')
+    const titulo = document.createElement('h2')
+    titulo.textContent = filme.nome 
+
+    container.appendChild(titulo)
+    
+    return card
+
+}
+
+function preencherContainer(){
+
+        const container = document.querySelector('body')
+const teste = {
+nome: " 10 coisas que eu odeio em você"
+}
+
+criarCard (teste)
+
+}
+
+console.table(await getFilme(1))
